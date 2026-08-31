a40c1d183dec90d09d6acaaea0c1cc61c948fbd4
Robin73389 <12229@holbertonstudents.com>
2026-08-31 12:40:00 +0200

    File executable with print différent information

---
+++ python_fundamentals/hello_world/structured_output.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+Langue = "Python"
+Version = 3
+Pi = 3.14
+Computation = True
+
+print(f"Language: {Langue}")
+print(f"Version: {Version}")
+print(f"Pi approx: {Pi:.2f}")
+print(f"Computation valid: {Computation}")
