01367e8fa7245f2b1cbb751438a65107c767466b
Robin73389 <12229@holbertonstudents.com>
2026-08-31 15:27:02 +0200

    I handle the negatives

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -4,9 +4,11 @@ number = __import__('random').randint(-10000, 10000)
 
 n = abs(number) % 10
 
+if number < 0:
+    n = -n
 if n >= 5:
     print(f"Last digit of {number} is {n} and is greater than 5")
 if n == 0:
     print(f"Last digit of {number} is {n} and is 0")
-if n < 6 and n > 0:
+if n < 6 and n != 0:
     print(f"Last digit of {number} is {n} and is less than 6 and not 0")
