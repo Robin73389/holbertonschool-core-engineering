5b79e6389a129a7aa2ca2bc25c902f147895c6cb
Robin73389 <12229@holbertonstudents.com>
2026-08-31 13:38:26 +0200

    Script wich check the last number and of the number

---
+++ python_fundamentals/control_flow/positive_or_negative.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+number = __import__('random').randint(-10000, 10000)
+
+last = number % 10
+
+if number < 0:
+    last = -last
+
+if last > 5:
+    print(f"Last digit of {number} is {last} and is greater than 5")
+
+if last == 0:
+    print(f"Last digit of {number} is {last} and is 0")
+
+if last < 6 and last > 0:
+    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
