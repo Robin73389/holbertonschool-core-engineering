b9599388fea9f67e4216364db51b9972514e1159
Robin73389 <12229@holbertonstudents.com>
2026-08-31 13:50:26 +0200

    Check if number is positif or negatif or == 0

--- python_fundamentals/control_flow/positive_or_negative.py
+++ python_fundamentals/control_flow/positive_or_negative.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
-number = __import__('random').randint(-10000, 10000)
 
-last = number % 10
+number = __import__('random').randint(-10, 10)
 
 if number < 0:
-    last = -last
+    print(f"{number} is negative")
 
-if last > 5:
-    print(f"Last digit of {number} is {last} and is greater than 5")
+if number == 0:
+    print(f"{number} is zero")
 
-if last == 0:
-    print(f"Last digit of {number} is {last} and is 0")
-
-if last < 6 and last > 0:
-    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
+if number > 0:
+    print(f"{number} is positive")
