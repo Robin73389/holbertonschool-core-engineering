b03c1933c6649d283965026e827883d7e3ce44b6
Robin73389 <12229@holbertonstudents.com>
2026-09-01 10:31:23 +0200

    I have add gomma

--- python_fundamentals/control_flow/print_comb3.py
+++ python_fundamentals/control_flow/print_comb3.py
@@ -3,4 +3,7 @@
 for i in range(0, 10):
     for j in range(0, 10):
         if i < j:
-            print("{}{}".format(i, j), end=" ")
+            if i == 8 and j == 9:
+                print("{}{}".format(i, j), end="")
+            else:
+                print("{}{}".format(i, j), end=", ")
