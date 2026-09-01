d7726ceeb2d0d13e9e9e465a6e3ba1578bb15d1f
Robin73389 <12229@holbertonstudents.com>
2026-09-01 12:53:55 +0200

    I delete the print in __name__

--- python_fundamentals/functions_modules/print_last_digit.py
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 
 def print_last_digit(number):
+    """_summary_
+
+    Args:
+        number (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     last_digit = abs(number) % 10
 
+    print(last_digit)
     return last_digit
 
 
 if __name__ == '__main__':
-    print(print_last_digit(-1024))
+    print_last_digit(98)
