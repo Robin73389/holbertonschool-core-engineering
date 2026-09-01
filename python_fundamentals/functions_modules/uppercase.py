4050711dd64e6a72f72ece5515ccfcba3bb6e805
Robin73389 <12229@holbertonstudents.com>
2026-09-01 12:21:28 +0200

    Function convert string lower in upper

---
+++ python_fundamentals/functions_modules/uppercase.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+
+def uppercase(str):
+    """_summary_
+
+    Args:
+        str (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    result = ""
+
+    for i in str:
+        if 'a' <= i <= 'z':
+            result += chr(ord(i) - 32)
+        else:
+            result += i
+
+    return result
+
+
+if __name__ == '__main__':
+    print(uppercase("holberton"))
