53557b51b4a56b16d196ce9ebf3f49b941c9ffb1
Robin73389 <12229@holbertonstudents.com>
2026-09-02 15:14:47 +0200

    Print list

---
+++ python_fundamentals/core_data_structures/print_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def print_list_integer(my_list=[]):
+    """_summary_
+
+    Args:
+        my_list (list, optional): _description_. Defaults to [].
+    """
+    for i in my_list:
+        print("{:d}".format(i))
