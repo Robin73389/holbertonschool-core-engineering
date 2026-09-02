fea9303ee39f3a81bad38b3d7955576eb6bff68c
Robin73389 <12229@holbertonstudents.com>
2026-09-02 10:33:33 +0200

    I import add_0

--- python_fundamentals/functions_modules/add.py
+++ python_fundamentals/functions_modules/add.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python3
 
-def add(a, b):
-    """_summary_
 
-    Args:
-        a (_type_): _description_
-        b (_type_): _description_
-    """
+import add_0
+
+if __name__ == '__main__':
+    a = 1
+    b = 2
     result = a + b
     print("{} + {} = {} ".format(a, b, result))
-
-
-if __name__ == "__main__":
-    add(1, 2)
