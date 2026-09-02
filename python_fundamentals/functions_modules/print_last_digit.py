61f0da13ca0d136b1dd0a51904c746c9538bd167
Robin73389 <12229@holbertonstudents.com>
2026-09-02 10:08:32 +0200

    delete return ligne

--- python_fundamentals/functions_modules/print_last_digit.py
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -11,9 +11,9 @@ def print_last_digit(number):
     """
     last_digit = abs(number) % 10
 
-    print("{}".format(last_digit))
+    print("{}".format(last_digit), end="")
     return last_digit
 
 
 if __name__ == '__main__':
-    print_last_digit(98)
+    print_last_digit(-1024)
