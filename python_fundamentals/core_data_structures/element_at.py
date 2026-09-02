4fd3b670d27647db5bcbdd562d820660cdcf2d88
Robin73389 <12229@holbertonstudents.com>
2026-09-02 15:24:32 +0200

    Find the element in list

---
+++ python_fundamentals/core_data_structures/element_at.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def element_at(my_list, idx):
+    """_summary_
+
+    Args:
+        my_list (_type_): _description_
+        idx (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    if idx < 0 or idx > len(my_list):
+        return None
+    else:
+        return my_list[idx]
