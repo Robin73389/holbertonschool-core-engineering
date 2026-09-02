7bd3da8206cfa288a428cb719952a485bc268e9d
Robin73389 <12229@holbertonstudents.com>
2026-09-02 10:21:56 +0200

    Program wich import function add

---
+++ python_fundamentals/functions_modules/add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def add(a, b):
+    """_summary_
+
+    Args:
+        a (_type_): _description_
+        b (_type_): _description_
+    """
+    result = a + b
+    print("{} + {} = {} ".format(a, b, result))
+
+
+if __name__ == "__main__":
+    add(1, 2)
