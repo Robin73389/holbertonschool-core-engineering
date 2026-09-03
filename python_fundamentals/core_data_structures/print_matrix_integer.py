8d7b8eaa09473a305926081879ce2373bc2ddbb3
Robin73389 <12229@holbertonstudents.com>
2026-09-03 12:19:41 +0200

    Display matrix 2D

--- python_fundamentals/core_data_structures/print_matrix_integer.py
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -8,5 +8,5 @@ def print_matrix_integer(matrix=[[]]):
     """
     for i in matrix:
         for c in i:
-            print("{:d}".format(c), end=" ")
+            print("{}".format(c), end=' ')
         print()
