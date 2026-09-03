a801b381680edca0fe8c8d1a96775db5981e3efc
Robin73389 <12229@holbertonstudents.com>
2026-09-03 12:24:35 +0200

    Check whether or not a space is needed.

--- python_fundamentals/core_data_structures/print_matrix_integer.py
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -8,5 +8,8 @@ def print_matrix_integer(matrix=[[]]):
     """
     for i in matrix:
         for c in i:
-            print("{:d}".format(c), end=' ')
+            if c != i[-1]:
+                print("{:d}".format(c), end=' ')
+            else:
+                print("{:d}".format(c), end='')
         print()
