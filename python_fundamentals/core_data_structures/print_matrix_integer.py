4b8f89976ba1e49a4d5b93a80e3513afd2890312
Robin73389 <12229@holbertonstudents.com>
2026-09-03 10:36:24 +0200

    Display matrix in 2D

---
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def print_matrix_integer(matrix=[[]]):
+    """_summary_
+
+    Args:
+        matrix (list, optional): _description_. Defaults to [[]].
+    """
+    for i in matrix:
+        for c in i:
+            print(c, end=" ")
+        print()
