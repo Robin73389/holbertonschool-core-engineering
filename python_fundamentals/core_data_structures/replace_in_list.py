5a1d2ea9021cb4f50145bc36799daf981efee6ff
Robin73389 <12229@holbertonstudents.com>
2026-09-03 09:54:47 +0200

    I added greater than or equal to to my condition.

--- python_fundamentals/core_data_structures/replace_in_list.py
+++ python_fundamentals/core_data_structures/replace_in_list.py
@@ -11,9 +11,10 @@ def replace_in_list(my_list, idx, element):
     Returns:
         _type_: _description_
     """
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return my_list
 
     else:
         my_list[idx] = element
-        return my_list
+
+    return my_list
