110437800892d25afc403587baa4359457123a84
Robin73389 <12229@holbertonstudents.com>
2026-09-03 15:32:55 +0200

    returns a set of common elements in two sets

---
+++ python_fundamentals/core_data_structures/common_elements.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def common_elements(set_1, set_2):
+    """_summary_
+
+    Args:
+        set_1 (_type_): _description_
+        set_2 (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    new_set = set_1
+
+    return new_set.intersection(set_2)
