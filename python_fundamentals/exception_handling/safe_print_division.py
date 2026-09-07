e88fd6059183b9b46119f414c3d5ed61fab73eb3
Robin73389 <12229@holbertonstudents.com>
2026-09-07 11:45:01 +0200

    Correction Inside result

--- python_fundamentals/exception_handling/safe_print_division.py
+++ python_fundamentals/exception_handling/safe_print_division.py
@@ -10,6 +10,6 @@ def safe_print_division(a, b):
         pass
 
     finally:
-        print("Inside result {}".format(result))
+        print("Inside result: {}".format(result))
 
     return result or None
