99f78357731c5a9130c5a8fdf0dace97746e2443
Robin73389 <12229@holbertonstudents.com>
2026-09-08 09:49:20 +0200

    Create private attribute

---
+++ python_oop/classes_and_object_model/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Square class"""
+
+
+class Square():
+    def __init__(self, size):
+        """_summary_
+
+        Args:
+            size (_type_): _description_
+        """
+        self.__size = size
