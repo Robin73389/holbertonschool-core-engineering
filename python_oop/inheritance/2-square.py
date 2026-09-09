23dc8929cc326f76ff13e59e8e2614d065c3a89a
Robin73389 <12229@holbertonstudents.com>
2026-09-09 15:37:27 +0200

    I add print method

---
+++ python_oop/inheritance/2-square.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+"""BaseGeomtry"""
+
+class BaseGeometry:
+    """Clas BaseGeometry"""
+    def integer_validator(self, name, value):
+        """_summary_
+
+        Args:
+            name (_type_): _description_
+            value (_type_): _description_
+
+        Raises:
+            TypeError: _description_
+            ValueError: _description_
+        """
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+    def area(self):
+        """_summary_
+
+        Raises:
+            Exception: _description_
+        """
+        raise Exception("area() is not implemented")
+
+
+class Rectangle(BaseGeometry):
+    """Class Rectangle"""
+    def __init__(self, width, height):
+        """_summary_
+
+        Args:
+            width (_type_): _description_
+            height (_type_): _description_
+        """
+        self.integer_validator("height", height)
+        self.integer_validator("width", width)
+
+        self.__width = width
+        self.__height = height
+
+
+class Square(Rectangle):
+    """_summary_
+
+    Args:
+        Rectangle (_type_): _description_
+    """
+    def __init__(self, size):
+        """_summary_
+
+        Args:
+            size (_type_): _description_
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size,size)
+
+    def area(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        return self.__size * self.__size
+
+    def print():
+        """_summary_
+        """
+        print()
+
+    def __str__(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        return f"[Square] {self.__size}/{self.__size}"
