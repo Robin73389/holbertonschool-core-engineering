3187fe919be57bbad832557a542ff578f99e3ca2
Robin73389 <12229@holbertonstudents.com>
2026-09-10 09:46:53 +0200

    Create Abstract method and subclas

---
+++ python_oop/abc/animals.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""ABC class"""
+from abc import ABC, abstractclassmethod
+
+
+# Crée une classe abstract Animal
+class Animal(ABC):
+    """_summary_
+
+    Args:
+        ABC (_type_): _description_
+    """
+    @abstractclassmethod
+    def sound(self):
+        """_summary_
+        """
+        pass
+
+
+# Crée une sous class Dog
+class Dog(Animal):
+    """_summary_
+
+    Args:
+        Animal (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    # Implémenté une method sound()
+    def sound(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        # return "Bark"
+        return "Bark"
+
+
+# Crée une sous class Cat
+class Cat(Animal):
+    """_summary_
+
+    Args:
+        Animal (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    # Implémenté une method sound()
+    def sound():
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        # return "Meow"
+        return "Meow"
