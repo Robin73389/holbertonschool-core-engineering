80605817c91dad9cfbd414538b700acd6c6c5036
Robin73389 <12229@holbertonstudents.com>
2026-09-11 18:20:41 +0200

    The mystical Dragon

---
+++ python_oop/abc/dragon.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Dragon"""
+
+
+class SwimMixin:
+    """_summary_
+    """
+    def swim(self):
+        """_summary_
+        """
+        print("The creature swims")
+
+
+class FlyMixin:
+    """_summary_
+    """
+    def fly(self):
+        """_summary_
+        """
+        print("The creature flies!")
+
+
+class Dragon(SwimMixin, FlyMixin):
+    """_summary_
+
+    Args:
+        SwimMixin (_type_): _description_
+        FlyMixin (_type_): _description_
+    """
+    def roar(self):
+        """_summary_
+        """
+        print("The dragon roars!")
