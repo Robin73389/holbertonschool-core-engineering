3ab19bdc819e44c01114d5aa5d52cb30542f5ff3
Robin73389 <12229@holbertonstudents.com>
2026-09-11 10:31:47 +0200

    FlyingFish

---
+++ python_oop/abc/flyingfish.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""Flyingish"""
+
+from abc import ABC, abstractmethod
+
+
+class Fish:
+    """Class Fish"""
+    def swim(self):
+        """Method swim"""
+        print("The fish is swimming")
+
+    def habitat(self):
+        """Method habitat"""
+        print("The fish lives in water")
+
+
+class Bird:
+    """Class Bird"""
+    def fly(self):
+        """Method fly"""
+        print("The bird is flying")
+
+    def habitat(self):
+        """Method habitat"""
+        print("The bird lives in the sky")
+
+
+class FlyingFish(Fish, Bird):
+    """Class FlyingFish"""
+    def fly(self):
+        """Method fly"""
+        print("The flying fish is soaring!")
+
+    def swim(self):
+        """Method swim"""
+        print("The flying fish is swimming!")
+
+    def habitat(self):
+        """Method habitat"""
+        print("The flying fish lives both in water and the sky!")
